4a7d5317662e1bfed1b789d39580af9d2479b192
gleachkr <gleachkr@gmail.com>
2017-08-01 10:58:12 -0600

    silence linter

--- dist/bespoke-synchro.js
+++ dist/bespoke-synchro.js
@@ -7,17 +7,11 @@
  */
 
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var n;"undefined"!=typeof window?n=window:"undefined"!=typeof global?n=global:"undefined"!=typeof self&&(n=self);var o=n;o=o.bespoke||(o.bespoke={}),o=o.plugins||(o.plugins={}),o.synchro=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+/*jshint bitwise:false*/
+
 module.exports = function(options) {
   function synchro(deck) {
     options = options || {};
-
-    var control = options.control || local;
-    var port = options.port || ~~(location.search.split('?')[1]) || 9999;
-    var url = options.url || (/https/.test(location.protocol) ? 'wss' : 'ws') 
-      + '://' + (location.hostname || 'localhost');
-
-    var path = options.path ? options.path : '';
-    url += ':' + port + '/' + path;
 
     function local(origin) {
       var parse = document.createElement('a');
@@ -25,8 +19,16 @@ module.exports = function(options) {
       var hostname = parse.hostname;
       return {
         local: hostname === 'localhost' || hostname === '127.0.0.1'
-      }
+      };
     }
+
+
+    var control = options.control || local;
+    var port = options.port || ~~(location.search.split('?')[1]) || 9999;
+    var url = options.url || (/https/.test(location.protocol) ? 'wss' : 'ws') + '://' + (location.hostname || 'localhost');
+
+    var path = options.path ? options.path : '';
+    url += ':' + port + '/' + path;
 
     var socket = new WebSocket(url);
 
@@ -41,7 +43,7 @@ module.exports = function(options) {
       if (synch()) {return;}
       if (!control(url, socket).local) {return;}
       if (!synchro.sending) { return; }
-      e.eventname = n 
+      e.eventname = n;
 
       socket.send(JSON.stringify(e)); 
     }
@@ -53,43 +55,43 @@ module.exports = function(options) {
     socket.onmessage = function (e) {
       if (!synchro.receiving) {return;}
       synch.ronizing = true;
-      ev = JSON.parse(e.data)
-      if (ev.eventname == 'next') {deck.next();};
-      if (ev.eventname == 'prev') {deck.prev();};
+      var ev = JSON.parse(e.data);
+      if (ev.eventname == 'next') {deck.next();}
+      if (ev.eventname == 'prev') {deck.prev();}
       if (ev.eventname == 'activate' && deck.slide() != ev.index) {
           deck.slide(ev.index);
-      };
+      }
       
     };
 
-  };
+  }
 
   synchro.receiving = true;
   synchro.sending = true;
 
-  function disable() { disable.rx(); disable.tx() }
+  function disable() { disable.rx(); disable.tx(); }
   disable.rx = function () { 
-    synchro.receiving = false
-  }
+    synchro.receiving = false;
+  };
   disable.tx = function () { 
-    synchro.sending = false
-  }
+    synchro.sending = false;
+  };
 
-  function enable() { enable.rx(); enable.tx() }
+  function enable() { enable.rx(); enable.tx(); }
   enable.rx = function () {
-    synchro.receiving = true
-  }
+    synchro.receiving = true;
+  };
   enable.tx = function () {
-    synchro.sending = true
-  }
+    synchro.sending = true;
+  };
 
-  function toggle() { toggle.rx(); toggle.tx() }
+  function toggle() { toggle.rx(); toggle.tx(); }
   toggle.rx = function () { 
-    synchro.receiving = !synchro.receiving
-  }
+    synchro.receiving = !synchro.receiving;
+  };
   toggle.tx = function () {
-    synchro.sending = !synchro.sending
-  }
+    synchro.sending = !synchro.sending;
+  };
 
 
   synchro.disable = disable;
